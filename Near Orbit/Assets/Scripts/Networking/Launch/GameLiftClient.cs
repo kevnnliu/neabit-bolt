dc7bd56cf6262fa0aa9c594e937557d989e8fdf2
Kevin Liu <kdliu00@gmail.com>
2020-04-24 17:56:23 -0700

    Restructured Launcher

--- Near Orbit/Assets/Scripts/Networking/Launch/GameLiftClient.cs
+++ Near Orbit/Assets/Scripts/Networking/Launch/GameLiftClient.cs
@@ -1,10 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Aws.GameLift.Server;
 
 public class GameLiftClient : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,4 +16,10 @@ public class GameLiftClient : MonoBehaviour
     {
         
     }
+
+    public void CreateClientConfiguration()
+    {
+
+    }
+
 }
--- Near Orbit/Assets/Scripts/Networking/Launch/Launcher.cs
+++ Near Orbit/Assets/Scripts/Networking/Launch/Launcher.cs
@@ -1,149 +1,20 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
-using System;
 using UdpKit;
-using UnityEngine.SceneManagement;
-using Bolt;
 using Bolt.Matchmaking;
 using Bolt.Photon;
-using UdpKit.Platform.Photon;
 
 public class Launcher : Bolt.GlobalEventListener
 {
-    enum State
-    {
-        SelectMode,
-        SelectMap,
-        SelectRoom,
-        StartServer,
-        StartClient,
-        Started,
-    }
 
-    Rect labelRoom = new Rect(0, 0, 140, 75);
-    GUIStyle labelRoomStyle;
-
-    State state;
-    string map;
+    public static string Username;
+    public static string UserID;
+    public static Dictionary<string, object> UserData;
 
     void Awake()
     {
         Application.targetFrameRate = 60;
-
-        labelRoomStyle = new GUIStyle()
-        {
-            fontSize = 20,
-            fontStyle = FontStyle.Bold,
-            normal =
-            {
-                textColor = Color.white
-            }
-        };
-    }
-
-    void OnGUI()
-    {
-        Rect tex = new Rect(10, 10, 140, 75);
-        Rect area = new Rect(10, 90, Screen.width - 20, Screen.height - 100);
-
-        GUI.Box(tex, Resources.Load("BoltLogo") as Texture2D);
-
-        if (BoltNetwork.IsRunning)
-        {
-            tex = new Rect(160, 10, 140, 75);
-            GUILayout.BeginArea(tex);
-            if (GUILayout.Button("Shutdown", GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true)))
-            {
-                BoltNetwork.Shutdown();
-            }
-            GUILayout.EndArea();
-        }
-
-        GUILayout.BeginArea(area);
-
-        switch (state)
-        {
-            case State.SelectMode: State_SelectMode(); break;
-            case State.SelectMap: State_SelectMap(); break;
-            case State.SelectRoom: State_SelectRoom(); break;
-            case State.StartClient: State_StartClient(); break;
-            case State.StartServer: State_StartServer(); break;
-        }
-
-        GUILayout.EndArea();
-    }
-
-    void State_SelectRoom()
-    {
-        GUI.Label(labelRoom, "Looking for rooms:", labelRoomStyle);
-
-        if (BoltNetwork.SessionList.Count > 0)
-        {
-            GUILayout.BeginVertical();
-            GUILayout.Space(30);
-
-            foreach (var session in BoltNetwork.SessionList)
-            {
-                var photonSession = session.Value as PhotonSession;
-
-                if (photonSession.Source == UdpSessionSource.Photon)
-                {
-                    var matchName = photonSession.HostName;
-                    var label = string.Format("Join: {0} | {1}/{2}", matchName, photonSession.ConnectionsCurrent, photonSession.ConnectionsMax);
-
-                    if (ExpandButton(label))
-                    {
-                        BoltMatchmaking.JoinSession(photonSession);
-                        state = State.Started;
-                    }
-                }
-            }
-
-            GUILayout.EndVertical();
-        }
-    }
-
-    void State_SelectMode()
-    {
-        if (ExpandButton("Server"))
-        {
-            state = State.SelectMap;
-        }
-        if (ExpandButton("Client"))
-        {
-            state = State.StartClient;
-        }
-    }
-
-    void State_SelectMap()
-    {
-        GUILayout.BeginVertical();
-
-        foreach (string value in BoltScenes.AllScenes)
-        {
-            if (SceneManager.GetActiveScene().name != value)
-            {
-                if (ExpandButton(value))
-                {
-                    map = value;
-                    state = State.StartServer;
-                }
-            }
-        }
-
-        GUILayout.EndVertical();
-    }
-
-    void State_StartServer()
-    {
-        BoltLauncher.StartServer();
-        state = State.Started;
-    }
-
-    void State_StartClient()
-    {
         BoltLauncher.StartClient();
-        state = State.SelectRoom;
     }
 
     public override void BoltStartBegin()
@@ -161,8 +32,9 @@ public class Launcher : Bolt.GlobalEventListener
         if (meta.TryGetValue("Data", out customData))
         {
             var text = "";
+            UserData = (Dictionary<string, object>)customData;
 
-            foreach (var item in (Dictionary<string, object>) customData)
+            foreach (var item in UserData)
             {
                 text += string.Format("{0} : {1}\n", item.Key, item.Value);
             }
@@ -171,53 +43,24 @@ public class Launcher : Bolt.GlobalEventListener
         }
 
         // Read the UserId of the local player
-        object userID;
-        if (meta.TryGetValue("UserId", out userID))
+        if (meta.TryGetValue("UserId", out object userID))
         {
-            BoltLog.Info("UserID: {0}", (string) userID);
+            UserID = (string)userID;
+            BoltLog.Info("UserID: {0}", UserID);
         }
 
         // Read the Nickname of the local player
-        object nickName;
-        if (meta.TryGetValue("Nickname", out nickName))
+        if (meta.TryGetValue("Nickname", out object nickName))
         {
-            BoltLog.Info("Nickname: {0}", (string) nickName);
+            Username = (string)nickName;
+            BoltLog.Info("Nickname: {0}", Username);
         }
 
         // Your usual BoltStartDone behaviour: setup game server or join a session
-
-        if (BoltNetwork.IsServer)
-        {
-            var id = Guid.NewGuid().ToString().Split('-')[0];
-            var matchName = string.Format("{0} - {1}", id, map);
-
-            BoltMatchmaking.CreateSession(
-                sessionID: matchName,
-                sceneToLoad: map
-            );
-        }
     }
 
     public override void BoltStartFailed(UdpConnectionDisconnectReason disconnectReason)
     {
         Debug.LogErrorFormat("BoltStartFailed. Reason: {0}", disconnectReason);
-    }
-
-    public override void BoltShutdownBegin(AddCallback registerDoneCallback, UdpConnectionDisconnectReason disconnectReason)
-    {
-        registerDoneCallback(() =>
-        {
-            state = State.SelectMode;
-        });
-    }
-
-    bool ExpandButton(string text)
-    {
-        return GUILayout.Button(text, GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true));
-    }
-
-    public override void SessionListUpdated(Map<Guid, UdpSession> sessionList)
-    {
-        BoltLog.Info("New session list");
     }
 }
